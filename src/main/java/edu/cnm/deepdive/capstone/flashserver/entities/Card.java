ac6a20f68a1379bddbbf947ce185fb2c2306d0fa
Ana <fhresak@gmail.com>
2017-11-30 09:40:38 -0700

    Added Card entity.

---
+++ src/main/java/edu/cnm/deepdive/capstone/flashserver/entities/Card.java
@@ -0,0 +1,98 @@
+package edu.cnm.deepdive.capstone.flashserver.entities;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+
+@Entity
+public class Card {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private long id;
+
+  private Date created;
+
+  private String front;
+
+  private String back;
+
+  private String imageOne;
+
+  private String imageTwo;
+
+  private String imageThree;
+
+  private String imageFour;
+
+  @OneToMany(mappedBy = "deck")
+  private Deck deck;
+
+  public String getFront() {
+    return front;
+  }
+
+  public void setFront(String front) {
+    this.front = front;
+  }
+
+  public String getBack() {
+    return back;
+  }
+
+  public void setBack(String back) {
+    this.back = back;
+  }
+
+  public String getImageOne() {
+    return imageOne;
+  }
+
+  public void setImageOne(String imageOne) {
+    this.imageOne = imageOne;
+  }
+
+  public String getImageTwo() {
+    return imageTwo;
+  }
+
+  public void setImageTwo(String imageTwo) {
+    this.imageTwo = imageTwo;
+  }
+
+  public String getImageThree() {
+    return imageThree;
+  }
+
+  public void setImageThree(String imageThree) {
+    this.imageThree = imageThree;
+  }
+
+  public String getImageFour() {
+    return imageFour;
+  }
+
+  public void setImageFour(String imageFour) {
+    this.imageFour = imageFour;
+  }
+
+  public Deck getDeck() {
+    return deck;
+  }
+
+  public void setDeck(Deck deck) {
+    this.deck = deck;
+  }
+  public long getId() {
+    return id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+}
