a0fbbd2534507e64f1597d9314635cb56b082666
Ana <fhresak@gmail.com>
2017-12-01 10:15:30 -0700

    Added Achievement Controller

---
+++ src/main/java/edu/cnm/deepdive/capstone/flashserver/controllers/AchievementController.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.capstone.flashserver.controllers;
+
+import edu.cnm.deepdive.capstone.flashserver.entities.Achievement;
+import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "achievements" , path = "achievements")
+public interface AchievementController extends PagingAndSortingRepository<Achievement,Long> {
+
+  List<Achievement> findByuniqueId(@Param("uniqueId") String uniqueId);
+
+}
